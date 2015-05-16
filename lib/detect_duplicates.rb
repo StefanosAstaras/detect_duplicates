83d8dde29aa44227e4d0eb4d5e64d3c2ea58e190
Stefanos Astaras <stefanosastaras@gmail.com>
2015-05-16 16:55:37 +0300

    Added usage()

--- lib/detect_duplicates.rb
+++ lib/detect_duplicates.rb
@@ -1,2 +1,14 @@
 #!/usr/local/bin/ruby -w
 require 'digest/md5'
+
+def usage()
+  puts "Detect duplicate files"
+  puts "Usage:"
+  puts "[ruby ] detect_duplicates.rb [max_bytes_read] [top_directory]"
+  puts "  max_bytes_read is the maximum number of bytes that the script will read from each file"
+  puts "    default: 2000"
+  puts "    use 0 to read entire files"
+  puts "  top_directory is the directory from which the script will start its recursive search"
+  puts "    default: current directiry"
+  puts
+end
